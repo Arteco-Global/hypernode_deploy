7b3a36ec148506540eabebdc9e38d4ced3df2eca
MDalprato <marcodalprato@me.com>
2024-09-04 12:44:28 +0200

    aggiunto script di installazione

--- install_hypernode.sh
+++ install_hypernode.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# Messaggio di benvenuto
+echo "Welcome to Hypernode installation process"
 
 # Chiedi all'utente di inserire la porta NON sicura
 read -p "HTTP server port: " SERVER_PORT
 
 # Chiedi all'utente di inserire la porta sicura
 read -p "HTTPS server port: " SSL_PORT
+
+# Attendi un invio
+read -p "Press Enter to continue with the installation..."
 
 # Imposta le variabili d'ambiente
 export SERVER_PORT=${SERVER_PORT:-80}
