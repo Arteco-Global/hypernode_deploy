6512784b11aa4f32586bdcaff8b0fba79ba097a2
MDalprato <marcodalprato@me.com>
2024-09-04 14:32:38 +0200

    Aggiunta opzione rabbit esterno

--- install_hypernode.sh
+++ install_hypernode.sh
@@ -8,12 +8,16 @@ read -p "HTTP server port: " SERVER_PORT
 # Chiedi all'utente di inserire la porta sicura
 read -p "HTTPS server port: " SSL_PORT
 
+# Chiedi all'utente di inserire la porta sicura
+read -p "Rabbit url: " RMQ
+
 # Attendi un invio
 read -p "Press Enter to continue with the installation..."
 
 # Imposta le variabili d'ambiente
 export SERVER_PORT=${SERVER_PORT:-80}
 export SSL_PORT=${SSL_PORT:-443}
+export RMQ=${RMQ:-amqp://hypernode:hypernode@rabbitmqHypernode:5672}
 
 # Esegui docker compose up -d
 docker compose up -d --build
