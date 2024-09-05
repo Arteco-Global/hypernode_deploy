2426640b4f3bff54745fdb6420f13869eaf2e402
MDalprato <marcodalprato@me.com>
2024-09-05 12:51:26 +0200

    spostato percorso sh

--- docker/nginx-docker.sh
+++ docker/nginx-docker.sh
@@ -3,7 +3,7 @@
 #!/bin/sh
 
 # Costruisci l'immagine Docker
-docker build -t nginx-local-purpose -f nginx.dockerfile .
+docker build -t nginx-local-purpose -f ../nginx.dockerfile .
 
 # Esegui il container Docker
 docker run -d --name nginxForLocalPurpose --network bridge -p 80:80 -p 443:443 nginx-local-purpose
