1d7cc224d8a5b0d8825c62a343e70426cfa47bbd
MDalprato <marcodalprato@me.com>
2024-11-11 12:37:31 +0100

    Fixies

--- install_hypernode.sh
+++ install_hypernode.sh
@@ -11,22 +11,12 @@ read -p "HTTPS server port: " SSL_PORT
 # Chiedi all'utente di inserire la porta sicura
 read -p "Rabbit url: " RMQ
 
-# Chiedi all'utente se è un aggiornamento
-read -p "Rebuil all the images? [Y/N]: " IS_UPDATE
-
 # Attendi un invio
 read -p "Press Enter to continue with the installation..."
 
 # Imposta le variabili d'ambiente
 export SERVER_PORT=${SERVER_PORT:-80}
 export SSL_PORT=${SSL_PORT:-443}
-export RMQ=${RMQ:-amqp://hypernode:hypernode@rabbitmqHypernode:5672}
+export RMQ=${RMQ:-amqp://hypernode:hypernode@messageBroker:5672}
 
-# Controlla se è un aggiornamento
-if [[ $IS_UPDATE == "Y" || $IS_UPDATE == "y" ]]; then
-    # Esegui docker compose up -d --build
-    docker compose up -d --build
-else
-    # Esegui docker compose up -d senza --build
-    docker compose up -d
-fi
+ docker compose up -d --build
