5043aebde3cb81ac2aca7b93afc4c48610e32b6a
MDalprato <marcodalprato@me.com>
2024-11-12 10:44:44 +0100

    Updated deploy

--- install_hypernode.sh
+++ install_hypernode.sh
@@ -3,13 +3,16 @@
 echo "Welcome to Hypernode installation process"
 
 # Chiedi all'utente di inserire la porta NON sicura
-read -p "HTTP server port: " SERVER_PORT
+read -p "HTTP server port (default 80): " SERVER_PORT
 
 # Chiedi all'utente di inserire la porta sicura
-read -p "HTTPS server port: " SSL_PORT
+read -p "HTTPS server port (default 443): " SSL_PORT
 
 # Chiedi all'utente di inserire la porta sicura
-read -p "Rabbit url: " RMQ
+read -p "Configurator port (default 8080): " CONF_PORT
+
+# Chiedi all'utente di inserire la porta sicura
+read -p "Rabbit url (edit only if remote): " RMQ
 
 # Attendi un invio
 read -p "Press Enter to continue with the installation..."
@@ -17,6 +20,7 @@ read -p "Press Enter to continue with the installation..."
 # Imposta le variabili d'ambiente
 export SERVER_PORT=${SERVER_PORT:-80}
 export SSL_PORT=${SSL_PORT:-443}
+export CONF_PORT=${CONF_PORT:-8080}
 export RMQ=${RMQ:-amqp://hypernode:hypernode@messageBroker:5672}
 
  docker compose up -d --build
