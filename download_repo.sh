01fd3a15f37df918c2e36593be81240219364935
MDalprato <marcodalprato@me.com>
2024-11-14 14:48:50 +0100

    fix download repo

--- download_repo.sh
+++ download_repo.sh
@@ -6,6 +6,7 @@ mkdir -p hypernode
 # Spostarsi dentro la cartella 'hypernode'
 cd hypernode
 
+#scadono tra 60 giorni
 usr=mdalprato
 psw=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
 
@@ -16,19 +17,4 @@ git clone https://$usr:$psw@github.com/Arteco-Global/hypernode_server_gui.git
 
 echo "Cloning procedure compleated"
 
-# Chiedere all'utente se vuole installare Hypernode
-read -p "Do you want to install Hypernode (yes/no): " INSTALLL
-
-# Leggere il valore della variabile INSTALLL
-echo "You selected: $INSTALLL"
-
-# Eseguire un'azione basata sulla risposta
-if [[ "$INSTALLL" == "yes" ]]; then
-  echo "Installing Hypernode..."
-  cd hypernode/hypernode_deploy
-  source install_hypernode.sh
-
-  # Qui puoi inserire il codice per installare Hypernode
-else
-  echo "Installation skipped."
-fi
+source ./hypernode_deploy/install_hypernode.sh
