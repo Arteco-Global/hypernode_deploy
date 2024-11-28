4d865847e683c13605ba893fd30882a133cb94c4
MDalprato <marcodalprato@me.com>
2024-11-28 15:29:28 +0100

    Added rabbit

--- docker/rabbit-docker.sh
+++ docker/rabbit-docker.sh
@@ -3,4 +3,4 @@ docker run -d --name rabbitForLocalPurpose -e RABBITMQ_DEFAULT_USER=hypernode -e
 
 #docker run -d --name rabbitdiff -e RABBITMQ_DEFAULT_USER=hypernode -e RABBITMQ_DEFAULT_PASS=hypernode -p 9000:5672 -p 8585:15672 rabbitmq:3.13-management
 
-# docker run  -e RABBITMQ_URI=amqp://hypernode:hypernode@192.168.10.31:5672 -e DATABASE_URI=mongodb://database:27017/camera-db2 --rm hypernode-server-camera 
+# docker run  -e RABBITMQ_URI=amqp://hypernode:hypernode@172.17.0.1:5672 -e DATABASE_URI=mongodb://172.17.0.1:27017/camera-db2 --rm hypernode-server-camera 
