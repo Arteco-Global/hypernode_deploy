bf3aa2f903d89c0bd9f1f8ce423cd192da6b63dd
LucaArteco <lvolta@arteco-global.com>
2024-12-10 17:13:48 +0100

    rimuovo erase db da instll demo

--- installer_7/install_demo.sh
+++ installer_7/install_demo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo bash installer.sh -skip-clean -skip-git-install -skip-docker-install -skip-clone -skip-branch-ask -erase-db
+sudo bash installer.sh -skip-clean -skip-clone -skip-git-install -skip-docker-install -skip-branch-ask
