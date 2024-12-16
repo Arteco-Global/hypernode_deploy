4ebf2214b61256c9c00718b0928e3b2d339470a2
MDalprato <marcodalprato@me.com>
2024-12-16 15:11:14 +0100

    test vari hproxy e rabbit

--- tools/rabbit_tester.py
+++ tools/rabbit_tester.py
@@ -1,7 +1,7 @@
 import pika
 
 # Stringa di connessione AMQP
-# url = "amqp://hypernode:hypernode@127.0.0.1:5672"
+# url = "amqp://hypernode:hypernode@127.0.0.1:5672
 url = "amqp://hypernode:hypernode@127.0.0.1:90"
 
 
