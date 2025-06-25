778ffc193a0e8defc1378c716cf7852a0f6ce04c
MDalprato <marcodalprato@me.com>
2025-06-25 12:11:38 +0200

    install gui

---
+++ installer_docker/setup_gui.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+zenity --question \
+  --title="uSS installer" \
+  --text="Do you want to proceed with the installation?" \
+  --ok-label="Install" \
+  --cancel-label="Close"
+
+# Check exit status
+if [ $? -eq 0 ]; then
+  zenity --info --text="Installation started!"
+  # Put your install command here, for example:
+  curl -sSL -o installer.sh https://raw.githubusercontent.com/Arteco-Global/hypernode_deploy/refs/heads/main/installer_docker/installer.sh
+  chmod +x installer.sh
+  sudo ./installer.sh -fi
+
+else
+  zenity --info --text="Installation cancelled."
+fi
